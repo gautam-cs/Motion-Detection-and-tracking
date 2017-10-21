fcf3f9ec3a45a31ebc0e88becbdfad11b34ad217
rptgautam <rptgautamkr@gmail.com>
2017-10-21 16:05:25 +0530

    working background substraction

--- object.py
+++ object.py
@@ -6,7 +6,7 @@ def get_frames_and_background_substration(cwd):
     vidcap = cv2.VideoCapture(os.path.join(cwd,'TownCentreXVID.avi'))
     success=True
     count = 0
-    val = 10000
+    # val = 10000
     while success:
         # vidcap.set(cv2.CAP_PROP_POS_MSEC, val)
         success,image = vidcap.read()
@@ -21,15 +21,21 @@ def get_frames_and_background_substration(cwd):
             previous_bitmap=current_bitmap
             # current_bitmap = cv2.imread(fr, 0)
             current_bitmap=image
-            diff = cv2.absdiff(current_bitmap, previous_bitmap)
-            cv2.imshow("current_bitmap", diff)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+            diff = cv2.absdiff( previous_bitmap, current_bitmap)
+            # thresh = cv2.threshold(diff, 25, 255, cv2.THRESH_BINARY)[1]
+            # thresh = cv2.dilate(thresh, None, iterations=2)
+            # im2, cnts, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            # temp = 0
+            # min_area = (3000 / 800) * current_bitmap.shape[1]
+            # for c in cnts:
+            #     # if the contour is too small, ignore it
+            #     if cv2.contourArea(c) > min_area:
+            #         temp = 1
+            cv2.imshow("image", diff)
+            cv2.waitKey(1)
         # val = val + 10000
         count += 1
-
-
-
+    cv2.destroyAllWindows()
 
 if __name__=="__main__":
     cwd = os.getcwd()
